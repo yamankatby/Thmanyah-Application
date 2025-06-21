d6102b58dbd595421f6e0de76984fc07e8945956
Yaman Katby <m.yaman.katby@gmail.com>
2025-06-21 12:11:06 +0300

    feat: add default redirect

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -5,6 +5,15 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [new URL("https://is1-ssl.mzstatic.com/**/*")],
   },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/search",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
